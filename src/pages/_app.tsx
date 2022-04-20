f8366eece728c52e93da92e48a9efe3394f0e725
katayama <tattu.0310@gmail.com>
2022-04-20 22:02:08 +0900

    🎨 タイトルタグ追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <>
+      <Head>
+        <title>Invoice</title>
+      </Head>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
   );
 }
 
