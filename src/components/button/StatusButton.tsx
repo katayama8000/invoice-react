082f09b1fdf9350c22a162600ebc594f33f05327
katayama <tattu.0310@gmail.com>
2022-04-22 18:26:15 +0900

    ⚡️ tailwind.cssを動的に変える

--- src/components/button/StatusButton.tsx
+++ src/components/button/StatusButton.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@mantine/core";
 
 type Props = {
   title: "Pending" | "Paid" | "Draft";
 };
-
 type colorType = "red" | "green" | "gray" | undefined;
+type circleColor = "bg-red-500" | "bg-green-500" | "bg-gray-500" | undefined;
 
 export const StatusButton: React.VFC<Props> = ({ title }) => {
+  const [circleColor, setCircleColor] = useState<circleColor>();
   let color: colorType;
+  let style = `${circleColor} inline-block h-2 w-2 rounded-full mr-1`;
+
+  useEffect(() => {
+    if (color === "red") setCircleColor("bg-red-500");
+    if (color === "green") setCircleColor("bg-green-500");
+    if (color === "gray") setCircleColor("bg-gray-500");
+  }, [color]);
+
   switch (title) {
     case "Pending":
       color = "red";
@@ -23,7 +32,7 @@ export const StatusButton: React.VFC<Props> = ({ title }) => {
   }
   return (
     <Button variant="subtle" color={color}>
-      <span className="inline-block h-2 w-2 rounded-full bg-orange-600" />
+      <span className={style} />
       {title}
     </Button>
   );
